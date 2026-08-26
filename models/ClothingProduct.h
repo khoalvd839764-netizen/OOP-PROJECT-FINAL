4eadef7deac6d088179f0f919ccee0e4a2359830
huedk9541-wq <huedk9541@ut.edu.vn>
2026-08-26 19:01:50 +0700

    Enhance ClothingProduct class with size and material
    Added size and material attributes to ClothingProduct class with corresponding methods.

--- models/ClothingProduct.h
+++ models/ClothingProduct.h
@@ -2,9 +2,32 @@
 #define CLOTHING_PRODUCT_H
 
 #include "Product.h"
+#include <string>
+
+using namespace std;
 
 class ClothingProduct : public Product {
+private:
+    string size;
+    string material;
 
+public:
+    ClothingProduct(
+        string id = "",
+        string name = "",
+        double price = 0,
+        int stockQuantity = 0,
+        string size = "",
+        string material = ""
+    );
+
+    string getSize() const;
+    string getMaterial() const;
+
+    void displayInfo() const override;
+    double calculateFinalPrice() const override;
+    string getType() const override;
+    Product* clone() const override;
 };
 
 #endif

f3ea8f083e9e4abec8e7a662911d7913315c6574
huedk9541-wq <huedk9541@ut.edu.vn>
2026-08-26 19:06:52 +0700

    Update ClothingProduct.cpp

--- models/ClothingProduct.cpp
+++ models/ClothingProduct.cpp
@@ -1,1 +1,51 @@
 #include "ClothingProduct.h"
+#include <iostream>
+
+using namespace std;
+
+ClothingProduct::ClothingProduct(
+    string id,
+    string name,
+    double price,
+    int stockQuantity,
+    string size,
+    string material
+)
+    : Product(id, name, "Clothing", price, stockQuantity),
+      size(size),
+      material(material)
+{
+}
+
+string ClothingProduct::getSize() const {
+    return size;
+}
+
+string ClothingProduct::getMaterial() const {
+    return material;
+}
+
+void ClothingProduct::displayInfo() const {
+    cout << "[CLOTHING] "
+         << getName()
+         << " | Giá VND: " << getPrice()
+         << " | Size: " << size
+         << " | Chất liệu: " << material
+         << endl;
+}
+
+double ClothingProduct::calculateFinalPrice() const {
+    if (material == "Silk" || material == "Leather") {
+        return getPrice() * 1.10;
+    }
+
+    return getPrice();
+}
+
+string ClothingProduct::getType() const {
+    return "CLOTHING";
+}
+
+Product* ClothingProduct::clone() const {
+    return new ClothingProduct(*this);
+}
